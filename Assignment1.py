61c502fd6699fac1aa55dc3ec863666f66a5d0fc
Ansh5678 <anshajgarg2005@gmail.com>
2025-09-20 21:19:30 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,14 @@
+print("Assignment 1st by Bhumika Choudhary")
+
+a=int(input("Enter the first Number: "))
+b=int(input("Enter the second Number: "))
+add= a + b
+sub= a - b
+mul= a * b
+div= a / b
+print("Addition =",add)
+print("Subtraction =",sub)
+print("Multiplication =",mul)
+print("Division =",div)
+
+
